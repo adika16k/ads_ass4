c979b4c8bb063d5d93bdd26aecb6b7cdf0fa07d2
adika16k <adilhankenzaliev@gmail.com>
2023-05-13 09:28:44 +0600

    ass4

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -14,5 +14,32 @@ public class MyHashTable<K, V> {
             return "{" + key + " " + value + "}";
         }
     }
+    private HashNode<K, V>[] chainArray;
+    private int M = 11;
+    private int size;
 
+    public MyHashTable(){
+
+    }
+    public MyHashTable(int M) {
+
+    }
+    private int Hash(K key){
+
+    }
+    public void put(K key, V value) {
+
+    }
+    public V get(K key) {
+
+    }
+    public V remove(K key) {
+
+    }
+    public boolean contains(V value) {
+
+    }
+    public K getKey(V value) {
+
+    }
 }
